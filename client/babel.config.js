0bbe0674a6bf5d5baa8643f7fbc69ab9301b4034
shafiq <quickserve494@gmail.com>
2024-01-16 14:08:45 +0530

    Signup and login UI created

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,6 +1,16 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      "@babel/plugin-proposal-export-namespace-from",
+      "react-native-reanimated/plugin",
+      "react-native-paper/babel",
+    ],
+    env: {
+      production: {
+        plugins: ["react-native-paper/babel"],
+      },
+    },
   };
 };
---
+++ client/screens/Signup.js
@@ -0,0 +1,70 @@
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { Button, Divider, TextInput } from "react-native-paper";
+import Animated, {
+  SlideInDown,
+  SlideInLeft,
+  SlideOutDown,
+} from "react-native-reanimated";
+const Signup = () => {
+  return (
+    <View style={styles.container}>
+      <View style={styles.titlecenter}>
+        <Text entering={SlideInLeft} style={styles.title}>
+          Signup
+        </Text>
+      </View>
+      <Divider style={styles.divider} />
+      <TextInput
+        label="Name"
+        mode="outline"
+        style={[styles.input, { marginTop: "20px" }]}
+      />
+      <TextInput label="Email" mode="outline" style={styles.input} />
+
+      <TextInput label="password" mode="outline" style={styles.input} />
+      <TextInput label="confirm password" mode="outline" style={styles.input} />
+      <Button
+        style={styles.btn}
+        mode="contained"
+        onPress={() => {
+          console.log("btn pressed");
+        }}
+      >
+        Signup
+      </Button>
+    </View>
+  );
+};
+
+export default Signup;
+
+const styles = StyleSheet.create({
+  container: {
+    width: "90%",
+    borderWidth: "1px",
+    padding: "20px",
+    borderRadius: "10px",
+  },
+  input: {
+    marginBottom: "20px",
+  },
+  divider: {
+    height: "2px",
+    marginTop: "10px",
+  },
+  title: {
+    fontSize: "35px",
+    fontStyle: "italic",
+    fontWeight: "bold",
+  },
+  titlecenter: {
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btn: {
+    paddingVertical: "10px",
+    borderRadius: "10px",
+  },
+});
